c71662207bf9516ec416fd7f078e3b046cb87164
pengpai <1171155957@qq.com>
2019-06-11 14:50:46 +0800

    init1

--- src/router.js
+++ src/router.js
@@ -1,25 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/Index/index.vue'),
+      children: [
+        {
+          path: 'movie',
+          name: 'movie',
+          component: () => import('./views/Index/movie.vue')
+        },
+        {
+          path: 'cinema',
+          name: 'cinema',
+          component: () => import('./views/Index/cinema.vue')
+        },
+        {
+          path: 'center',
+          name: 'center',
+          component: () => import('./views/Index/center.vue')
+        },
+        {
+          path: '',
+          redirect: '/movie'
+        }
+      ]
     }
   ]
 })
---
+++ src/views/Index/movie.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class='nav-header'>猫眼电影</h1>
+</template>
+
+
