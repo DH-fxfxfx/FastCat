32fb21f50372ce13395150bc5326e3458c668e43
dh <327422733@qq.com>
2019-06-13 15:02:55 +0800

    定位

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store/store.js'
 import './styles/common/reset.less'
+import { Swipe, SwipeItem, Tab, Tabs, NavBar, IndexBar, IndexAnchor, Cell } from 'vant'
+Vue
+  .use(Swipe)
+  .use(SwipeItem)
+  .use(Tab)
+  .use(Tabs)
+  .use(NavBar)
+  .use(IndexBar)
+  .use(IndexAnchor)
+  .use(Cell)
+
 Vue.config.productionTip = false
 
 new Vue({
